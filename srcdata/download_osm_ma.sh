92ff89b240d6c045ac625e51802b98abdcb29e8e
jremillard <remillard.jason@gmail.com>
2013-03-23 18:35:40 -0400

    update download script to use new url

--- srcdata/download_osm_ma.sh
+++ srcdata/download_osm_ma.sh
@@ -1,5 +1,5 @@
 # script to download current MA OSM data, and replace OSM data in postGIS database
 rm massachusetts.osm.bz2
-wget http://download.geofabrik.de/openstreetmap/north-america/us/massachusetts.osm.bz2 massachusetts.osm.bz2
+wget http://download.geofabrik.de/north-america/us/massachusetts-latest.osm.bz2 massachusetts.osm.bz2
 rm index.html
 
